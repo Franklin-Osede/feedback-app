dc16eaa37ad18c52a696e8a80a6640e22289bb85
Franklin <71532004+fran1014@users.noreply.github.com>
2023-08-05 01:44:28 +0200

    Creating of feedbac from with typing

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import FeedBackItem from './FeedbackItem';
+
+function FeedbackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback Yet</p>;
+  }
+  return (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedBackItem key={item.id} item={item} handleDelete={handleDelete} />
+      ))}
+    </div>
+  );
+}
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+export default FeedbackList;
